7e204d33b785ed4b9a4398c1879b79d4bbd98dc0
Marek Socha <maresocha@gmail.com>
2017-08-28 00:58:34 +0200

    Added config scripts

---
+++ game.js
@@ -0,0 +1,11 @@
+var Startup = (function () {
+    function Startup() {
+    }
+    Startup.main = function () {
+        console.log('Hello hgfhgfdsfd das dsds adf hg f');
+        return 0;
+    };
+    return Startup;
+}());
+Startup.main();
+//# sourceMappingURL=game.js.map
---
+++ game.ts
@@ -0,0 +1,8 @@
+class Startup {
+    public static main(): number {
+        console.log('Hello hgfhgfdsfd das dsds adf hg f');
+        return 0;
+    }
+}
+
+Startup.main(); 
